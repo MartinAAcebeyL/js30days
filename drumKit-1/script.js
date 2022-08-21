4662181f354d7e18ac118b6d4cd8266b515f248e
MartinAAcebeyL <martinaacbyl2000@gmail.com>
2022-08-21 14:07:10 -0400

    reto 1 acabado

--- drumKit-1/script.js
+++ drumKit-1/script.js
@@ -1,1 +1,22 @@
- const teclas = document.getElementsByClassName('');
+const keys = Array.from(document.querySelectorAll('.key'))
+
+keys.forEach(element =>{
+    element.addEventListener("transitionend", eliminar_transicion)
+});
+
+function eliminar_transicion(e) {
+    e.target.classList.remove('playing');
+}
+
+
+function tecla_presionada(e) {
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if(key){
+        const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        key.classList.add('playing');
+        audio.currentTime = 0;//para moverse a un segungo del audio que indiques
+        audio.play();
+    } 
+}
+
+window.addEventListener("keydown", tecla_presionada);
